44e2a90b40ddd0700a09b98dffdbda4dff664f0e
Shantu Das <shantudasbd@yahoo.com>
2019-01-02 16:30:30 +0600

    MainActivity some cooments added with progress dialog

--- app/src/main/java/com/snipex/shantu/androidretrofitwithjava/view/MainActivity.java
+++ app/src/main/java/com/snipex/shantu/androidretrofitwithjava/view/MainActivity.java
@@ -1,10 +1,12 @@
 package com.snipex.shantu.androidretrofitwithjava.view;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.snipex.shantu.androidretrofitwithjava.R;
@@ -21,11 +23,14 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
     private static final String TAG = MainActivity.class.getSimpleName();
+    private ProgressDialog progressDialog;
     private RecyclerView my_recycler_view;
     private LinearLayoutManager layoutManager;
     private ArticleAdapter adapter;
     private ArrayList<Article> articleArrayList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,9 +40,13 @@ public class MainActivity extends AppCompatActivity {
 
         getMoviesArticle();
 
-
     }
 
+    /**
+     * All views and others initialization
+     *
+     * @param @null
+     */
     private void initialization() {
         my_recycler_view = (RecyclerView) findViewById(R.id.my_recycler_view);
 
@@ -48,24 +57,35 @@ public class MainActivity extends AppCompatActivity {
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         my_recycler_view.setHasFixedSize(true);
+
+        // Progress Dialog
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
     }
 
+    /**
+     * get movies article from https://newsapi.org/v2/everything/
+     * Note :: adding two queries to the base url. First, what type of article you want show. For example - movies, and Second, api key
+     * onResponse response dismiss progress dialog
+     * if response status found ok then set adapter to show all articles in RecyclerView
+     * onFailure response dismiss progress dialog and show "something went wrong" message to the user
+     *
+     * @param @null
+     */
     private void getMoviesArticle() {
+
         ApiRequest apiRequest = RetrofitRequest.getRetrofitInstance().create(ApiRequest.class);
+        Call<ArticleResponse> call = apiRequest.getMovieArticles(getResources().getString(R.string.api_query_movies), getResources().getString(R.string.api_key));
 
-
-        Call<ArticleResponse> call = apiRequest.getMovieArticles("movies", "079dac74a5f94ebdb990ecf61c8854b7");
         call.enqueue(new Callback<ArticleResponse>() {
             @Override
             public void onResponse(Call<ArticleResponse> call, Response<ArticleResponse> response) {
-
                 Log.d(TAG, "onResponse response:: " + response);
-
                 if (response.body() != null) {
-
-                    articleArrayList = new ArrayList<>(response.body().getArticles());
-
-                    if (response.body().getStatus().equals("ok")){
+                    if (response.body().getStatus().equals("ok")) {
+                        progressDialog.dismiss();
+                        articleArrayList = new ArrayList<>(response.body().getArticles());
                         // specify an adapter
                         adapter = new ArticleAdapter(MainActivity.this, articleArrayList);
                         my_recycler_view.setAdapter(adapter);
@@ -76,9 +96,10 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<ArticleResponse> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
                 Log.d(TAG, "onFailure response :: " + t);
             }
-
         });
     }
 }
